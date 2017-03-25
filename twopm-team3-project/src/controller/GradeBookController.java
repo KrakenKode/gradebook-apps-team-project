62949d2d7fd78ae53a0361ef9067c0f66d0411b6
resonatur <resonatur@localhost>
2017-03-25 13:48:41 -0500

    Added inner classes for view listeners.

--- twopm-team3-project/src/controller/GradeBookController.java
+++ twopm-team3-project/src/controller/GradeBookController.java
@@ -2,28 +2,52 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import model.*;
 import view.*;
 
 public class GradeBookController implements ActionListener {
-
-	private GradeBookModel model;
-	
 	
 	private GradeBookView view;
-	
-	
+	private GradeBookModel model;
 	
 	public GradeBookController(GradeBookModel model, GradeBookView view) {
 		this.model = model;
 		this.view = view;
-	}
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		//TODO functions for adding classes to view
 		
 	}
-
+	
+	public void loadData(){
+		
+	}
+	
+	class MenuListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String command = e.getActionCommand();
+			if (command.equals("Open")){
+				
+			} else if (e.equals("Save")){
+				
+			}
+		}
+	}
+	
+	class TreeListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String command = e.getActionCommand();
+		}
+	}
+	
+	class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String command = e.getActionCommand();
+			
+		}
+	}
+	
+	
 }
