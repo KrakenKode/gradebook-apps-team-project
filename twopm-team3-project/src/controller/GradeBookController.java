bcee796f1bcda8358530142c08efdb0d7f898c56
krakenkode <daltonsumrall@gmail.com>
2017-03-25 11:30:13 -0500

    Added controller, model, and view. Main now creates a window

---
+++ twopm-team3-project/src/controller/GradeBookController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.*;
+import view.*;
+
+public class GradeBookController implements ActionListener {
+
+	public GradeBookController(GradeBookModel model, GradeBookView view) {
+		
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- twopm-team3-project/src/main/GradeBook.java
+++ twopm-team3-project/src/main/GradeBook.java
@@ -1,9 +1,25 @@
 package main;
 
+import javax.swing.JFrame;
+
+import controller.*;
+import model.*;
+import view.*;
+
+
 public class GradeBook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		GradeBookModel model = new GradeBookModel();
+		GradeBookView view = new GradeBookView();
+		GradeBookController controller = new GradeBookController(model, view);
+		
+		//view.registerListener(controller);
+		
+		view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		view.setSize(1000, 1000);
+		view.setVisible(true);
 
 	}
 
