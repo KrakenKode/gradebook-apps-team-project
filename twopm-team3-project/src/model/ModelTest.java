207f1bf37a640c8027081148dbad0b166fb28b22
suprmel <melissa_glez1@hotmail.com>
2017-04-01 13:55:22 -0500

    gradebook summary label

--- twopm-team3-project/src/model/ModelTest.java
+++ twopm-team3-project/src/model/ModelTest.java
@@ -4,8 +4,6 @@ public class ModelTest {
 
 	public static void main(String[] args) {
 		GradeBookModel model = new GradeBookModel();
-		System.out.println("HELLLOOOO");
-		System.out.println("But why");//hjfgbn
 		for(Semester semester : model.getSemesters()) {
 			System.out.println(semester.getName());
 			
