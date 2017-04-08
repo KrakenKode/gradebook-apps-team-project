e0ecde1ad566c9d0a59902bccfa0728e43398b24
Gustavo Chavez <cascade144@live.com>
2017-04-08 17:48:56 -0500

    Changed the EditGradeRange class to use GridBag and added comments.

--- twopm-team3-project/src/view/EditGradeRangePopUp.java
+++ twopm-team3-project/src/view/EditGradeRangePopUp.java
@@ -1,6 +1,8 @@
 package view;
 
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -18,7 +20,10 @@ public class EditGradeRangePopUp {
 	}
 	
 	public void gradeRangePopUp() {
-		JPanel panel = new JPanel(new GridLayout(0, 2, 20, 2));
+		JPanel panel = new JPanel(new GridBagLayout());
+		//set up constraints
+		GridBagConstraints jLabelC = new GridBagConstraints();
+		GridBagConstraints tFieldC = new GridBagConstraints();
 		//Set up new labels
 		JLabel Alabel = new JLabel("A");
 		JLabel Blabel = new JLabel("B");
@@ -29,23 +34,42 @@ public class EditGradeRangePopUp {
 		JTextField Bfield = new JTextField(3);
 		JTextField Cfield = new JTextField(3);
 		JTextField Dfield = new JTextField(3);
-		//Set fields to current defualt grade ranges
+		//Set fields to current default grade ranges
 		Afield.setText(Integer.toString(course.getGradeRange("A")));
 		Bfield.setText(Integer.toString(course.getGradeRange("B")));
 		Cfield.setText(Integer.toString(course.getGradeRange("C")));
 		Dfield.setText(Integer.toString(course.getGradeRange("D")));
 		
-		panel.add(Alabel);
-		panel.add(Afield);
-		panel.add(Blabel);
-		panel.add(Bfield);
-		panel.add(Clabel);
-		panel.add(Cfield);
-		panel.add(Dlabel);
-		panel.add(Dfield);
-		
+		jLabelC.insets = new Insets(0, 0, 0, 20);
+		tFieldC.insets = new Insets(0, 10, 0, 0);
+		//Edit constraints and add JLabels and JTextFields
+		jLabelC.gridx = 0;
+		jLabelC.gridy = 0;
+		panel.add(Alabel, jLabelC);
+		tFieldC.gridx = 1;
+		tFieldC.gridy = 0; 
+		panel.add(Afield, tFieldC);
+		jLabelC.gridx = 0;
+		jLabelC.gridy = 1;
+		panel.add(Blabel, jLabelC);
+		tFieldC.gridx = 1;
+		tFieldC.gridy = 1; 
+		panel.add(Bfield, tFieldC);
+		jLabelC.gridx = 0;
+		jLabelC.gridy = 2;
+		panel.add(Clabel, jLabelC);
+		tFieldC.gridx = 1;
+		tFieldC.gridy = 2; 
+		panel.add(Cfield, tFieldC);
+		jLabelC.gridx = 0;
+		jLabelC.gridy = 3;
+		panel.add(Dlabel, jLabelC);
+		tFieldC.gridx = 1;
+		tFieldC.gridy = 3; 
+		panel.add(Dfield, tFieldC);
+		//Pop up dialog box
 		int result = JOptionPane.showConfirmDialog(null, panel, "Enter Grade Range Information", JOptionPane.OK_CANCEL_OPTION);
-		
+		//sets the grade ranges based off of what the user has input
 		if (result == JOptionPane.OK_OPTION) {
 			course.setGradeRange("A", Integer.parseInt(Afield.getText()));
 			course.setGradeRange("B", Integer.parseInt(Bfield.getText()));
