06062290d09078c74ff50192a55c3dae19a9fe35
fiq362 <austincarpenter0815@gmail.com>
2017-04-13 15:50:04 -0500

    Added GPA to Semester Report

--- twopm-team3-project/src/view/SemesterReportCard.java
+++ twopm-team3-project/src/view/SemesterReportCard.java
@@ -1,6 +1,13 @@
 package view;
 
 import model.*;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel; 
@@ -9,6 +16,8 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset; 
 import org.jfree.data.category.DefaultCategoryDataset; 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SemesterReportCard extends JFrame
 {
@@ -22,10 +31,17 @@ public class SemesterReportCard extends JFrame
          createDataset(sem),          
          PlotOrientation.VERTICAL,           
          true, true, false);
+      	double GPA = getGPA(sem);
          
       ChartPanel chartPanel = new ChartPanel( barChart );        
-      chartPanel.setPreferredSize(new java.awt.Dimension( 550 , 400 ) );
-      setContentPane( chartPanel );
+      chartPanel.setPreferredSize(new java.awt.Dimension( 750 , 600 ) );
+      JPanel panel = new JPanel(new BorderLayout());
+      JLabel GPAlabel = new JLabel(" Semester GPA = " + GPA);
+      GPAlabel.setFont(new Font(GPAlabel.getName(), Font.PLAIN, 16));
+      panel.add(chartPanel);
+      panel.add(GPAlabel, BorderLayout.SOUTH);
+      panel.setBackground(Color.WHITE);
+      setContentPane( panel );
    }
    private CategoryDataset createDataset(Semester sem)
    {
@@ -40,5 +56,19 @@ public class SemesterReportCard extends JFrame
 	  }
 	 
       return dataset; 
+   }
+   
+   private double getGPA(Semester sem) {
+	   double GPA;
+	   double total = 0;
+	   int cnt = 0;
+	   ArrayList<Course> courses = sem.getCourses();
+	   for (Course course : courses) {
+		   course.updatePercentage();
+		   total += (course.getPercentage()/20-1);
+		   cnt++;
+	   }
+	   GPA = total/cnt;
+	   return new BigDecimal(GPA).setScale(2, BigDecimal.ROUND_CEILING).doubleValue();
    }
 }
